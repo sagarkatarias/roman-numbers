6f2a25d92a0d36aaad7167f48287338673d572cd
sagarkatarias <sagarkataria45@gmail.com>
2024-02-28 16:10:40 +0530

    Add route converter

--- src/routes/Converter.jsx
+++ src/routes/Converter.jsx
@@ -1,10 +1,27 @@
 import React, { useState } from "react";
 
+import { binaryToRoman, decimalToRoman } from "../utils/Calculator";
+import Output from "../components/Output";
+import Input from "../components/Input";
+
 function Converter() {
-  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleConvert = (number, inputFormat, outputFormat) => {
+    let convertedResult = "";
+
+    if (inputFormat === "binary" && outputFormat === "roman") {
+      convertedResult = binaryToRoman(number);
+    } else if (inputFormat === "decimal" && outputFormat === "roman") {
+      convertedResult = decimalToRoman(number);
+    }
+    setResult(convertedResult);
+  };
   return (
     <>
-      Converter
+      <h1>Number Converter</h1>
+      <Input onConvert={handleConvert} />
+      <Output result={result} />
     </>
   );
 }
