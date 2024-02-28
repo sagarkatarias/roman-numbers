ff9439882ccbc7e080e57fe526ce92618fd103d8
sagarkatarias <sagarkataria45@gmail.com>
2024-02-28 17:38:07 +0530

    Add test for calculator

---
+++ src/tests/Calculator.test.js
@@ -0,0 +1,32 @@
+import { binaryToRoman, decimalToRoman } from '../utils/Calculator';
+
+describe('binaryToRoman function', () => {
+  test('converts binary "10111" to Roman numeral "XXIII"', () => {
+    expect(binaryToRoman('10111')).toBe('XXIII');
+  });
+
+  test('converts binary "1101" to Roman numeral "XIII"', () => {
+    expect(binaryToRoman('1101')).toBe('XIII');
+  });
+
+  test('converts binary "1001" to Roman numeral "IX"', () => {
+    expect(binaryToRoman('1001')).toBe('IX');
+  });
+
+});
+
+describe('decimalToRoman function', () => {
+    test('converts decimal "23" to Roman numeral "XXIII"', () => {
+      expect(decimalToRoman('23')).toBe('XXIII');
+    });
+  
+    test('converts decimal "45" to Roman numeral "XLV"', () => {
+      expect(decimalToRoman('45')).toBe('XLV');
+    });
+  
+    test('converts decimal "9" to Roman numeral "IX"', () => {
+      expect(decimalToRoman('9')).toBe('IX');
+    });
+  
+    // Add more test cases as needed
+});
